ffe8f640b7c5c0774526e2b16417c271825d83fc
a_yoshino <atsuko.yoshino@rakus.co.jp>
2021-08-09 22:39:25 +0900

    sam pipelineがうまくいかなかったので、直接codepipelineに環境構築

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
+                message: 'Good News!!!!!!!!',
                 // location: ret.data.trim()
             })
         }

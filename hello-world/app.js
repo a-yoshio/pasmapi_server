6b2ba4cbc124decdc64b8998555c7e443fa6a7fd
a_yoshino <atsuko.yoshino@rakus.co.jp>
2021-08-09 15:54:57 +0900

    ci/cdテスト

--- hello-world/app.js
+++ hello-world/app.js
@@ -17,8 +17,6 @@ let response;
 exports.lambdaHandler = async (event, context) => {
     try {
         // const ret = await axios(url);
-        console.log('TEST konichiwa!')
-        console.log('TEST TEST!!!!')
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
